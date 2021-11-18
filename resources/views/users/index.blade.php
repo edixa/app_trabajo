fb698040ff1e735001c0d8c2fe5d999a00cd4a94
edixa <edixavillarreal170@gmail.com>
2021-11-18 13:41:50 -0600

    Finalizado, muestra usuarios y elimina

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -21,7 +21,7 @@
                             <td>{{ $usuario->email }}</td>
                             <td>
                                 <button class="btn btn-secondary"> 
-                                    <i class="fa fa-trash"></i> 
+                                    <i class="fa fa-trash"> Editar</i> 
                                 </button>
                                 |
                                 <form 
@@ -37,6 +37,7 @@
                                         value="delete"
                                         class="btn btn-danger"
                                     >
+                                    Eliminar
                                     </button>
                                 </form>
                             </td>
